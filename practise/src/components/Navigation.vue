f90f68fddcaa4900c57c96f2202204ea0e7c8952
KuznetsovMA <jztenk@gmail.com>
2019-01-17 19:02:26 +0300

    Navigation component

---
+++ practise/src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navigation">
+        <router-link to="/">Main</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/faq">FAQ</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navigation {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 200px;
+}
+</style>
