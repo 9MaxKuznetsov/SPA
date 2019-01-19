598de82a965b4c5fd820e9282e12a8c009509b9e
KuznetsovMA <jztenk@gmail.com>
2019-01-19 14:56:00 +0300

    Vuex state

--- practise/src/router.js
+++ practise/src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Userid from './views/Userid.vue'
 
 Vue.use(Router)
 
@@ -18,12 +16,10 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: About
-    },
-    {
-      path: '/id:id',
-      name: 'id',
-      component: Userid
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
